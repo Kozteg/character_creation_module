651ae61cfc8210fde10462292a46b0e79601e661
Константин Шмаков <koztig@gmail.com>
2023-01-25 12:50:33 +0300

    final edit

--- main.py
+++ main.py
@@ -2,40 +2,38 @@ from random import randint
 
 
 def attack(char_name, char_class):
-    if char_class == 'warrior':
-        dmg = 5 + randint(3, 5)
-        return (f'{char_name} нанёс урон противнику равный {dmg}')
-    if char_class == 'mage':
-        dmg = 5 + randint(5, 10)
-        return (f'{char_name} нанёс урон противнику равный {dmg}')
-    if char_class == 'healer':
-        dmg = 5 + randint(-3, -1)
-        return (f'{char_name} нанёс урон противнику равный {dmg}')
     dmg = 5
+    if char_class == 'warrior':
+        dmg += randint(3, 5)
+    elif char_class == 'mage':
+        dmg += randint(5, 10)
+    elif char_class == 'healer':
+        dmg += randint(-3, -1)
     return (f'{char_name} нанёс урон противнику равный {dmg}')
 
 
 def defence(char_name, char_class):
-    if char_class == 'warrior':
-        block = 10 + randint(5, 10)
-        return (f'{char_name} блокировал {block} урона')
-    if char_class == 'mage':
-        block = 10 + randint(-2, 2)
-        return (f'{char_name} блокировал {block} урона')
-    if char_class == 'healer':
-        block = 10 + randint(2, 5)
-        return (f'{char_name} блокировал {block} урона')
     block = 10
+    if char_class == 'warrior':
+        block += randint(5, 10)
+    elif char_class == 'mage':
+        block += randint(-2, 2)
+    elif char_class == 'healer':
+        block += randint(2, 5)
     return (f'{char_name} блокировал {block} урона')
 
 
 def special(char_name, char_class):
     if char_class == 'warrior':
-        return (f'{char_name} применил специальное умение «Выносливость {80 + 25}»')
+        sta = 80 + 25
+        return (f'{char_name} применил '
+                f'специальное умение «Выносливость {sta}')
     if char_class == 'mage':
-        return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
+        atk = 5 + 40
+        return (f'{char_name} применил специальное умение «Атака {atk}»')
     if char_class == 'healer':
-        return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
+        prot = 10 + 30
+        return (f'{char_name} применил специальное умение «Защита {prot}»')
     return (f'{char_name} не применил умение»')
 
 
@@ -71,12 +69,20 @@ def choice_char_class():
             'Введи название персонажа, за которого хочешь играть: '
             'Воитель — warrior, Маг — mage, Лекарь — healer: ')
         if char_class == 'warrior':
-            print('Воитель — дерзкий воин ближнего боя. Сильный, выносливый и отважный.')
+            print(
+                'Воитель — дерзкий воин ближнего боя. '
+                'Сильный, выносливый и отважный.')
         if char_class == 'mage':
-            print('Маг — находчивый воин дальнего боя. Обладает высоким интеллектом.')
+            print(
+                'Маг — находчивый воин дальнего боя. '
+                'Обладает высоким интеллектом.')
         if char_class == 'healer':
-            print('Лекарь — могущественный заклинатель. Черпает силы из природы, веры и духов.')
-        approve_choice = input('Нажми (Y), чтобы подтвердить выбор, или любую другую кнопку, чтобы выбрать другого персонажа ').lower()
+            print(
+                'Лекарь — могущественный заклинатель. '
+                'Черпает силы из природы, веры и духов.')
+        approve_choice = input(
+            'Нажми (Y), чтобы подтвердить выбор, или любую другую кнопку, '
+            'чтобы выбрать другого персонажа ').lower()
     return char_class
 
 
